70ea109845626ae0f747c127bef113d0df971d44
bcorrea <bcorrea@cs.washington.edu>
2024-08-01 15:48:05 -0700

    Build a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react"; // React imports first
-import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react"; // 3rd party imports next
-import App from "./App"; // Our own imports last
-import "./index.css";
+import React from 'react'; // React imports first
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react'; // 3rd party imports next
+import App from './App'; // Our own imports last
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
