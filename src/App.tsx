7ac5502507eaa5c244a3d758cc2546d7c353f6c1
bcorrea <bcorrea@cs.washington.edu>
2024-08-08 15:48:03 -0700

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,16 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`, // wider than 1024 px
       }}
+      templateColumns={{
+        base: '1fr', // 1 fraction AKA one column takes up all available space
+        lg: '200px 1fr', // 1st col is 200px, second takes all available space
+      }}
     >
       <GridItem area='nav'>
         <NavBar />
       </GridItem>
       <Show above='lg'>
-        <GridItem area='aside'>
+        <GridItem area='aside' paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
